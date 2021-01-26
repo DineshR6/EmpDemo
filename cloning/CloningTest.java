5960f284367e55d350871aa51bb5bd56d294312e
DineshR6 <44109728+DineshR6@users.noreply.github.com>
2021-01-26 14:52:54 +0530

    Update CloningTest.java

--- cloning/CloningTest.java
+++ cloning/CloningTest.java
@@ -18,7 +18,7 @@ public class CloningTest {
 	public static void main(String[] args) throws CloneNotSupportedException {
 
 		//create emp obj with data
-		Employee e1 = new Employee();
+		Employee e1 = new Employee(); //object creation
 		e1.setId(1);
 		e1.setName("shyam");
 
